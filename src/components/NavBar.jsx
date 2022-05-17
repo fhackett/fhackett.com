93845f2bfa71d88d9f9d0b913205a74891f21b19
fhackett <fhackett.py@gmail.com>
2022-05-16 20:56:01 -0700

    un-borked navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,13 +7,13 @@ export function NavBar() {
         <div class="navbar-brand">
             <a class="navbar-item" href="/">A. Finn Hackett<sub>(he/they)</sub>, MMath</a>
 
-            <a role="button" onClick={setNavbarExpanded(prev => !prev)} classList={{ 'is-active': navbarExpanded }} class="navbar-burger" aria-label="menu" aria-expanded={{navbarExpanded}}>
+            <a role="button" onClick={() => setNavbarExpanded(prev => !prev)} classList={{ 'is-active': navbarExpanded(), 'navbar-burger': true }} aria-label="menu" aria-expanded={navbarExpanded()}>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             </a>
         </div>
-        <div class="navbar-menu" classList={{ 'is-active': navbarExpanded }}>
+        <div class="navbar-menu" classList={{ 'is-active': navbarExpanded() }}>
             <div class="navbar-start">
             </div>
 
