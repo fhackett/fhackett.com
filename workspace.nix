ee94433f5bb6fd5b0d8d908136ece41ea77da040
Finn Hackett <fhackett.py@gmail.com>
2026-05-01 05:39:44 -0700

    couple spelling issues

--- workspace.nix
+++ workspace.nix
@@ -16,12 +16,14 @@ let
       pkgs.nix-vscode-extensions.open-vsx-release.scalameta.metals
       pkgs.nix-vscode-extensions.open-vsx-release.scala-lang.scala
       pkgs.nix-vscode-extensions.open-vsx-release.jnoortheen.nix-ide
+      pkgs.nix-vscode-extensions.open-vsx-release.ltex-plus.vscode-ltex-plus
     ];
   };
 in
   pkgs.mkShell {
     packages = [
       pkgs.javaPackages.compiler.openjdk25
+      pkgs.ltex-ls-plus
       vscodium
     ];
     setupHook = ''
